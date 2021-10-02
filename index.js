c104568cad9a386cf1645d908050377b7f7450b4
Alexandra Kostyuchenko <all2115@mail.ru>
2021-10-02 13:17:08 +0500

    fix lesson15 part2

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 'use strict'
 
 class First { 
-    constructor(text) {
+    constructor() {
         this.text = 'Привет, я метод родителя!'
     }
-        hello() {
-             console.log(this.text)
-        }   
+    hello() {
+        console.log(this.text)
+    }   
 }
-const first = new First();
 
 
 class Second extends First {
-    constructor(text) {
-        super(text);
-        this.text = 'А я наследуемый метод!'
-    } 
+    hello(){
+        
+        super.hello()
+
+        this.hello
+        console.log('А я наследуемый метод!')
+    }
 }
 let second = new Second();
-
-first.hello();
 second.hello();
